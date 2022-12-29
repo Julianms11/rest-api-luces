b44efeae45ad710e739573d41a39581366e6774a
Julianms11 <juliandavidms2004@gmail.com>
2022-12-29 18:03:15 -0500

    example

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 const cors = require("cors");
 
 // const whiteList = ['https://luces-navidad-frontend.vercel.app'];
 
 //settings
-app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2);
+app.set("port", process.env.PORT || 3001);
+app.set("json spaces", 2);
 
 //middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-
 //routes
-app.use('/api',require('./routes/index'))
+app.use("/api", require("./routes/index"));
 
 //Starting server
-app.listen(app.get('port'));
-console.log('Server on port: ', app.get('port'));
+app.listen(app.get("port"));
+console.log("Server on port: ", app.get("port"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,18 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const fs = require('fs');
+const fs = require("fs");
 
-
-router.get('/', (req,res) =>{
-    const file = fs.readFileSync('src/ledState.json', 'utf-8')
-    const jsonObj = JSON.parse(file);
-    res.json(jsonObj);
+router.get("/", (req, res) => {
+  const file = fs.readFileSync("src/ledState.json", "utf-8");
+  const jsonObj = JSON.parse(file);
+  res.json(jsonObj);
 });
-router.patch('/', (req,res)=>{
-    const newState = JSON.stringify(req.body);
-    fs.writeFileSync('src/ledState.json', newState, 'utf-8');
-    console.log(req.body);
-    res.json(req.body);
-})
 
+router.patch("/", (req, res) => {
+  const newState = JSON.stringify(req.body);
+  fs.writeFileSync("src/ledState.json", newState, "utf-8");
+  console.log(req.body);
+  res.json(req.body);
+});
 
 module.exports = router;
